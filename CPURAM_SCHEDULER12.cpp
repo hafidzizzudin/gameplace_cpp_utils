4c5446973b510971e35224ef147ddae2019dc20c
hafidz.izzudin <hafidz.izzudin49@gmail.com>
2020-12-24 12:02:45 +0700

    test 12.15, 12.30

--- CPURAM_SCHEDULER12.cpp
+++ CPURAM_SCHEDULER12.cpp
@@ -7,7 +7,7 @@
 #include <string>
 #include <sstream>
 
-bool isCritPerformance(double *cpu, double *ram)
+void isCritPerformance(double *cpu, double *ram)
 {
     char buffer[128];
     double cpu_used = 0.0, percent_ram;
@@ -88,21 +88,22 @@ bool isCritPerformance(double *cpu, double *ram)
     *cpu = cpu_used / 4.0;
     *ram = percent_ram;
 
-    if (percent_ram >= 70.0 || *cpu >= 70.0)
-    {
-        return true;
-    }
-    return false;
+    // printf("cpu: %f, ram: %f\n", *cpu, *ram);
+    // if (percent_ram >= 70.0 || *cpu >= 70.0)
+    // {
+    //     return true;
+    // }
+    // return false;
 }
 
-void sendWarningMsg(const char *currenttime, const double *cpu, const double *ram)
+void sendWarningMsg(const char *currenttime, const double &cpu, const double &ram)
 {
     std::ostringstream ss;
     char percent_stat[128];
-    sprintf(percent_stat, "CPU used: %.2f %%, RAM used: %.2f %%", *cpu, *ram);
+    sprintf(percent_stat, "AVGCPU used: %.2f %%, AVGRAM used: %.2f %%", cpu, ram);
 
     ss << "curl -H \"Content-Type: application/json\" --data";
-    ss << " '{\"chat_id\":-495061825, \"text\":\"[WARNING garudaprofit.gameplace.asia]\n"
+    ss << " '{\"chat_id\":-495061825, \"text\":\"[DAILY REPORT garudaprofit.gameplace.asia]\n"
        << currenttime << percent_stat << "\"}'";
     ss << " https://api.telegram.org/bot1220023412:AAF19w_vUpv2ctznq6HR88WxNIe_XMM8KK4/sendMessage";
     ss << " >>/dev/null 2>>/dev/null";
@@ -124,52 +125,88 @@ void checkLocalTime()
 
     FILE *log = fopen("check_cron.txt", "a");
 
+    printf("%d-%d-%d", localtm->tm_hour, localtm->tm_min, localtm->tm_sec);
+
     fprintf(log, asctime(localtm));
 }
 
 int main(int argc, char *argv[])
 {
-    checkLocalTime();
+    pid_t pid = fork();
 
-    // pid_t pid = fork();
+    if (pid > 0)
+    {
+        printf("Process is running in the background PID: %d\n", pid);
+        exit(0);
+    }
 
-    // if (pid > 0)
-    // {
-    //     printf("Process run in the background PID: %d\n", pid);
-    //     exit(0);
-    // }
+    int nCpu = 0;
+    int nRam = 0;
+    double totCpu = 0.0; // in percent
+    double totRam = 0.0; // in percent
 
-    // while (1)
-    // {
-    //     sleep(1);
-    //     double cpu_used, ram_used;
+    while (1)
+    {
+        sleep(1);
+        double cpu_used, ram_used;
 
-    //     if (isCritPerformance(&cpu_used, &ram_used))
-    //     {
-    //         time_t local;
-    //         struct tm *localtm;
+        isCritPerformance(&cpu_used, &ram_used);
+        totCpu += cpu_used;
+        totRam += ram_used;
 
-    //         time(&local);
-    //         localtm = localtime(&local);
+        nCpu++;
+        nRam++;
 
-    //         char filename[128], program[128];
-    //         strcpy(program, argv[0]);
-    //         memset(filename, 0x00, sizeof(filename));
-    //         sprintf(filename, "%s/%s%02d%02d.log", getenv("LOG_HOME"), argv[0],
-    //                 localtm->tm_mon + 1, localtm->tm_mday);
+        time_t local;
+        struct tm *localtm;
 
-    //         FILE *log = fopen(filename, "a");
-    //         if (log != NULL)
-    //         {
-    //             fprintf(log, "%s", asctime(localtm));
-    //             fprintf(log, "CPU used: %.2f %%\n", cpu_used);
-    //             fprintf(log, "RAM used: %.2f %%\n\n", ram_used);
+        time(&local);
+        localtm = localtime(&local);
 
-    //             sendWarningMsg(asctime(localtm), &cpu_used, &ram_used);
-    //         }
-    //         fclose(log);
-    //     };
-    // }
+        if ((localtm->tm_hour == 12 && localtm->tm_min == 15 && localtm->tm_sec == 0) ||
+            (localtm->tm_hour == 12 && localtm->tm_min == 30 && localtm->tm_sec == 0))
+        {
+            double avgCpu = 0.0, avgRam = 0.0;
+            
+            if (nCpu != 0)
+                avgCpu = (double)(totCpu / nCpu);
+            
+            if(nRam != 0)
+                avgRam = (double)(totRam / nRam);
+
+            sendWarningMsg(asctime(localtm), (double)(totCpu / nCpu), (double)(totRam / nRam));
+
+            totCpu = 0.0;
+            totRam = 0.0;
+            nCpu = 0;
+            nRam = 0;
+        }
+        // if (isCritPerformance(&cpu_used, &ram_used))
+        // {
+        //     time_t local;
+        //     struct tm *localtm;
+
+        //     time(&local);
+        //     localtm = localtime(&local);
+
+        //     char filename[128], program[128];
+        //     strcpy(program, argv[0]);
+        //     memset(filename, 0x00, sizeof(filename));
+        //     sprintf(filename, "%s/%s%02d%02d.log", getenv("LOG_HOME"), argv[0],
+        //             localtm->tm_mon + 1, localtm->tm_mday);
+
+        //     FILE *log = fopen(filename, "a");
+        //     if (log != NULL)
+        //     {
+        //         fprintf(log, "%s", asctime(localtm));
+        //         fprintf(log, "CPU used: %.2f %%\n", cpu_used);
+        //         fprintf(log, "RAM used: %.2f %%\n\n", ram_used);
+
+        //         sendWarningMsg(asctime(localtm), &cpu_used, &ram_used);
+        //     }
+        //     fclose(log);
+        // };
+    }
 
     return 0;
 }
